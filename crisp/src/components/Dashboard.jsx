38ab75c55b253a22c3a56d57a728c6b11cae1aa3
Durga Sankaran <155147768+DURGAS059@users.noreply.github.com>
2026-04-29 15:17:23 +0530

    Change welcome message from Durga to Dhivya

--- crisp/src/components/Dashboard.jsx
+++ crisp/src/components/Dashboard.jsx
@@ -8,7 +8,7 @@ export default function Dashboard({ username }) {
     <div className="dashboard-container">
       
       <section className="hero">
-        <h1>Welcome back Durga{username}!</h1>
+        <h1>Welcome back Dhivya{username}!</h1>
         <p>Access your recipes</p>
       </section>
 
